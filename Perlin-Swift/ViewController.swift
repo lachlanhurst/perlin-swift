b7d6c97d66f83b7c6c456a3915416e718f1273ae
Lachlan Hurst <lachlan@vpac.org>
2015-08-24 18:43:28 +1000

    Initial Commit

---
+++ Perlin-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Perlin-Swift
+//
+//  Created by Lachlan Hurst on 24/08/2015.
+//  Copyright (c) 2015 Lachlan Hurst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
